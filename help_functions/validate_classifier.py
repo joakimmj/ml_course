d58d1b7c032690901dbfe3bca3f17d419a30d75c
Joakim Myrvoll Johansen <joakimmyrvoll@gmail.com>
2017-08-29 21:26:38 +0200

    fix: fixed error with numb class

--- help_functions/validate_classifier.py
+++ help_functions/validate_classifier.py
@@ -37,9 +37,7 @@ def __split_data_set(data_set: np.ndarray, test_size: float):
     return data_set[:split], data_set[split:]
 
 
-def __execute(data_loader, feature_extractor, classifier, show: int, test_size: float, bitmap: bool, cahce_data: bool,
-              rows: int):
-    data, labels = data_loader(cache_data=cahce_data, rows=rows)
+def __execute(data, labels, feature_extractor, classifier, show: int, test_size: float, bitmap: bool):
     data, labels = utils.shuffle(data, labels)
 
     print('Extracting features...')
@@ -60,11 +58,13 @@ def __execute(data_loader, feature_extractor, classifier, show: int, test_size: 
 
 def execute_spam_filter(show: int = 10, test_size: float = .3, cache_data: bool = False, rows: int = -1):
     print('-- Executing spam filter')
-    __execute(data_retriever.load_sms, spam_filter.feature_extraction,
-              spam_filter.init_classifier, show, test_size, bitmap=False, cahce_data=cache_data, rows=rows)
+    data, labels = data_retriever.load_sms(cache_data=cache_data, rows=rows)
+    __execute(data, labels, spam_filter.feature_extraction,
+              spam_filter.init_classifier, show, test_size, bitmap=False)
 
 
-def execute_number_classifier(show: int = 10, test_size: float = .3, reload: bool = True, rows: int = -1):
+def execute_number_classifier(show: int = 10, test_size: float = .3, cache_data: bool = True, rows: int = -1):
     print('-- Executing number classification')
-    __execute(data_retriever.load_mnist, number_classifier.feature_extraction,
-              number_classifier.init_classifier, show, test_size, bitmap=True, cahce_data=reload, rows=rows)
+    data, labels = data_retriever.load_mnist()
+    __execute(data, labels, number_classifier.feature_extraction,
+              number_classifier.init_classifier, show, test_size, bitmap=True)
