db860077bf6958eb4bc1353e90ac9272ab5b3905
Joakim Myrvoll Johansen <joakimmyrvoll@gmail.com>
2017-08-29 13:22:21 +0200

    refactor: change docstrings to use "

--- tasks/sentiment_analysis.py
+++ tasks/sentiment_analysis.py
@@ -4,13 +4,13 @@ from sklearn.base import TransformerMixin, RegressorMixin
 
 
 def make_pipeline():
-    '''
+    """
     This is where we will build our pipeline.
     Pipelines are basically a chain of transformers followed by an estimator.
     The first transformer should be one (or more) methods of feature extraction.
 
     :return: a working pipeline.
-    '''
+    """
     pipeline_steps = [
         ('sfe', StupidFeatureExtractor()),
         ('svr', RandomRegressor())
@@ -20,41 +20,38 @@ def make_pipeline():
 
 
 class StupidFeatureExtractor(TransformerMixin):
-    '''
+    """
     Just a stupid transformer that takes in some data and returns garbage.
-
-    '''
+    """
 
     def __init__(self):
-        '''
+        """
         This is where you would accept the hyper parameters
-        '''
+        """
         pass
 
     def fit(self, documents, y=None):
         return self
 
     def transform(self, documents, y=None):
-        '''
+        """
         This does not actually transform the documents to anything.
         It just spits out a random matrix with the correct amount of rows and 5 cols.
-        '''
+        """
         return np.random.rand(len(documents), 5)
 
 
 class RandomRegressor(RegressorMixin):
-    '''
+    """
     Just a stupid predictor that takes in some data and returns garbage.
-
-    '''
+    """
 
     def __init__(self, minimum = 1, maximum = 11):
-        '''
+        """
         This is where you would accept the hyper parameters
-        '''
+        """
         self.minimum = minimum
         self.maximum = maximum
-
 
     def fit(self, X, y):
         pass
